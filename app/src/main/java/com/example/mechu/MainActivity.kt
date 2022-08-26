07c3fa435680e8f32227219b5c9e9a65757a3c2f
radic2682 <Hed2cns4me>
2022-08-27 04:05:43 +0900

    Commit 3

--- app/src/main/java/com/example/mechu/MainActivity.kt
+++ app/src/main/java/com/example/mechu/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -15,11 +16,21 @@ import androidx.core.content.ContextCompat
 import com.example.mechu.databinding.ActivityMainBinding
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
-
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private var getLatitude : Double? = null //위도
     private var getLongitude : Double? = null //경도
+
+    // REST API 키
+    companion object {
+        const val BASE_URL = "https://dapi.kakao.com/"
+        const val API_KEY = "KakaoAK 18385c4f27c0bc5b310a297d80a3fbed"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,6 +63,10 @@ class MainActivity : AppCompatActivity() {
                     mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(getLatitude!!, getLongitude!!), 3, true)
                     mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
 
+                    //지역 정보 받아오기
+                    searchKeyword()
+
+
                     //마커 설정
 
 
@@ -61,6 +76,7 @@ class MainActivity : AppCompatActivity() {
         }.launch("android.permission.ACCESS_FINE_LOCATION")
     }
 
+
     private fun missingPermisson(binding: ActivityMainBinding) {
         Toast.makeText(this, "앱 설정에서 위치 정보 제공을 동의해야\n" +
                 "정상적인 이용이 가능합니다." , Toast.LENGTH_LONG).show()
@@ -68,7 +84,6 @@ class MainActivity : AppCompatActivity() {
         binding.gameButton.visibility = View.INVISIBLE
         binding.warnText.visibility = View.VISIBLE
     }
-
 
     // 위치 권한 요청과 현재 위치 반환
     private fun setPosition() {
@@ -96,4 +111,31 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun searchKeyword() {
+        val retrofit = Retrofit.Builder()   // Retrofit 구성
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val api = retrofit.create(KakaoAPI::class.java)   // 통신 인터페이스를 객체로 생성
+        val call = api.getSearchCategory(API_KEY, "FD6", getLongitude.toString(), getLatitude.toString(), 500)   // 검색 조건 입력
+
+        // API 서버에 요청
+        call.enqueue(object: Callback<ResultSearchKeyword> {
+            override fun onResponse(
+                call: Call<ResultSearchKeyword>,
+                response: Response<ResultSearchKeyword>
+            ) {
+                // 통신 성공 (검색 결과는 response.body()에 담겨있음)
+                Log.d("Test", "Raw: ${response.raw()}")
+                Log.d("Test", "Body: ${response.body()}")
+            }
+
+            override fun onFailure(call: Call<ResultSearchKeyword>, t: Throwable) {
+                // 통신 실패
+                Log.w("MainActivity", "통신 실패: ${t.message}")
+            }
+        })
+    }
+
 }
